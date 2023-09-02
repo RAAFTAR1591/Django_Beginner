38c0af34e916601f43d8c5998e9317ff81df6c10
RAAFTAR1591 <swarajlahiri@gmail.com>
2023-09-02 10:56:05 +0530

    Updated Templates and Admin Page

--- Hello/Hello/urls.py
+++ Hello/Hello/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Portfolio Admin"
+admin.site.site_title = "Portfolio Admin Portal"
+admin.site.index_title = "Welcome to Portfolio Portal"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
